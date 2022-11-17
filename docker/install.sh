27896c4bc502b3ac4ffb874c107f58b7c58f9a79
Cody Garver <dylan.garver@leidos.com>
2022-11-17 09:24:46 -0500

    Simplify

--- docker/install.sh
+++ docker/install.sh
@@ -14,14 +14,14 @@ cp -R /home/carma/autoware.ai/jsk_recognition /home/carma/.base-image/ros2_msgs_
 rm -r /home/carma/autoware.ai/
 
 if [ "$DOCKERHUB_BUILD" == "true" ]; then
-  PARALLEL_WORKERS="--parallel-workers 4"
+  export ROS_PARALLEL_JOBS='-j4 -l4' # Try to reduce memory consumption on build
 fi
 
 # ROS1 message setup
-cd ~/.base-image/ros1_msgs_ws && source /opt/ros/noetic/setup.bash && colcon build $PARALLEL_WORKERS --packages-up-to autoware_msgs cav_msgs cav_srvs j2735_msgs can_msgs carma_debug_msgs autoware_lanelet2_msgs
+cd ~/.base-image/ros1_msgs_ws && source /opt/ros/noetic/setup.bash && colcon build --packages-up-to autoware_msgs cav_msgs cav_srvs j2735_msgs can_msgs carma_debug_msgs autoware_lanelet2_msgs
 
 # ROS2 message setup
-cd ~/.base-image/ros2_msgs_ws && source /opt/ros/foxy/setup.bash && colcon build $PARALLEL_WORKERS --packages-up-to autoware_msgs autoware_lanelet2_msgs can_msgs carma_debug_ros2_msgs carma_driver_msgs carma_localization_msgs carma_msgs carma_perception_msgs carma_planning_msgs carma_v2x_msgs j2735_v2x_msgs
+cd ~/.base-image/ros2_msgs_ws && source /opt/ros/foxy/setup.bash && colcon build --packages-up-to autoware_msgs autoware_lanelet2_msgs can_msgs carma_debug_ros2_msgs carma_driver_msgs carma_localization_msgs carma_msgs carma_perception_msgs carma_planning_msgs carma_v2x_msgs j2735_v2x_msgs
 
 # Build the bridge
 source /opt/ros/noetic/setup.bash 

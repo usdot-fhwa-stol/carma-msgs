fe9ccd9eab12560b1b77ec99b29cb28ab57cb80a
Cody Garver <dylan.garver@leidos.com>
2022-11-17 09:12:13 -0500

    Throttle dockerhub build

--- docker/install.sh
+++ docker/install.sh
@@ -13,11 +13,15 @@ cp -R /home/carma/autoware.ai/jsk_recognition /home/carma/.base-image/ros2_msgs_
 # Cleanup autoware repo once messages have been moved
 rm -r /home/carma/autoware.ai/
 
+if [ "$DOCKERHUB_BUILD" == "true" ]; then
+  PARALLEL_WORKERS="--parallel-workers 4"
+fi
+
 # ROS1 message setup
-cd ~/.base-image/ros1_msgs_ws && source /opt/ros/noetic/setup.bash && colcon build --packages-up-to autoware_msgs cav_msgs cav_srvs j2735_msgs can_msgs carma_debug_msgs autoware_lanelet2_msgs
+cd ~/.base-image/ros1_msgs_ws && source /opt/ros/noetic/setup.bash && colcon build $PARALLEL_WORKERS --packages-up-to autoware_msgs cav_msgs cav_srvs j2735_msgs can_msgs carma_debug_msgs autoware_lanelet2_msgs
 
 # ROS2 message setup
-cd ~/.base-image/ros2_msgs_ws && source /opt/ros/foxy/setup.bash && colcon build --packages-up-to autoware_msgs autoware_lanelet2_msgs can_msgs carma_debug_ros2_msgs carma_driver_msgs carma_localization_msgs carma_msgs carma_perception_msgs carma_planning_msgs carma_v2x_msgs j2735_v2x_msgs
+cd ~/.base-image/ros2_msgs_ws && source /opt/ros/foxy/setup.bash && colcon build $PARALLEL_WORKERS --packages-up-to autoware_msgs autoware_lanelet2_msgs can_msgs carma_debug_ros2_msgs carma_driver_msgs carma_localization_msgs carma_msgs carma_perception_msgs carma_planning_msgs carma_v2x_msgs j2735_v2x_msgs
 
 # Build the bridge
 source /opt/ros/noetic/setup.bash 
@@ -27,5 +31,5 @@ source ~/.base-image/ros2_msgs_ws/install/local_setup.bash
 cd ~/.base-image/workspace/src 
 cd ../ 
 sudo apt-get update 
-colcon build --packages-select ros1_bridge --cmake-args "--debug-output"
+colcon build $PARALLEL_WORKERS --packages-select ros1_bridge --cmake-args "--debug-output"
 sudo chmod -R ugo+x ~/.base-image/workspace/install

d8bca97cc65a9e1f243b7b843fc38310da2e2d3e
Cody Garver <dylan.garver@leidos.com>
2022-12-21 17:56:12 -0500

    Verbose logging

--- docker/install.sh
+++ docker/install.sh
@@ -31,5 +31,5 @@ source ~/.base-image/ros2_msgs_ws/install/local_setup.bash
 cd ~/.base-image/workspace/src 
 cd ../ 
 sudo apt-get update 
-colcon build --packages-select ros1_bridge --cmake-args "--debug-output"
+colcon build --event-handlers console_direct+ --packages-select ros1_bridge --cmake-args "--debug-output"
 sudo chmod -R ugo+x ~/.base-image/workspace/install

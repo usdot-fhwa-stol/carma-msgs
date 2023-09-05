4ed3b9df898340f19ff6de210fb1c3c855705ea5
Adam Morrissett <adam.l.morrissett@leidos.com>
2023-09-05 17:58:04 -0400

    #215 Add CP interfaces to install script
    Needed to add the carma_cooperated_perception_interfaces package to the
    installation script since the script use the --packages-up-to flag.

--- docker/install.sh
+++ docker/install.sh
@@ -16,18 +16,18 @@ cp -R /home/carma/autoware.ai/jsk_recognition /home/carma/.base-image/ros2_msgs_
 rm -rf /home/carma/autoware.ai/
 
 # ROS1 message setup
-cd ~/.base-image/ros1_msgs_ws && source /opt/ros/noetic/setup.bash && colcon build --packages-up-to autoware_msgs cav_msgs cav_srvs j2735_msgs can_msgs carma_debug_msgs autoware_lanelet2_msgs
+cd ~/.base-image/ros1_msgs_ws && source /opt/ros/noetic/setup.bash && colcon build --packages-up-to autoware_msgs cav_msgs cav_srvs j2735_msgs can_msgs carma_debug_msgs autoware_lanelet2_msgs carma_cooperative_perception_interfaces
 
 # ROS2 message setup
-cd ~/.base-image/ros2_msgs_ws && source /opt/ros/foxy/setup.bash && colcon build --packages-up-to autoware_msgs autoware_lanelet2_msgs can_msgs carma_debug_ros2_msgs carma_driver_msgs carma_localization_msgs carma_msgs carma_perception_msgs carma_planning_msgs carma_v2x_msgs j2735_v2x_msgs
+cd ~/.base-image/ros2_msgs_ws && source /opt/ros/foxy/setup.bash && colcon build --packages-up-to autoware_msgs autoware_lanelet2_msgs can_msgs carma_debug_ros2_msgs carma_driver_msgs carma_localization_msgs carma_msgs carma_perception_msgs carma_planning_msgs carma_v2x_msgs j2735_v2x_msgs carma_cooperative_perception_interfaces
 
 # Build the bridge
-source /opt/ros/noetic/setup.bash 
-source /opt/ros/foxy/setup.bash 
-source ~/.base-image/ros1_msgs_ws/install/local_setup.bash 
-source ~/.base-image/ros2_msgs_ws/install/local_setup.bash 
-cd ~/.base-image/workspace/src 
-cd ../ 
-sudo apt-get update 
+source /opt/ros/noetic/setup.bash
+source /opt/ros/foxy/setup.bash
+source ~/.base-image/ros1_msgs_ws/install/local_setup.bash
+source ~/.base-image/ros2_msgs_ws/install/local_setup.bash
+cd ~/.base-image/workspace/src
+cd ../
+sudo apt-get update
 colcon build --event-handlers console_direct+ --packages-select ros1_bridge --cmake-force-configure
 sudo chmod -R ugo+x ~/.base-image/workspace/install

c8007dac9de91f4e1c15913cd3308f01a9dc3814
Adam Morrissett <adam.l.morrissett@leidos.com>
2023-09-15 13:49:57 -0400

    #220 rename Python module
    The previous module name was too vague. The file_io name provides a
    better hint at the module's purpose.

--- carma_message_utilities/carma_message_utilities/__init__.py
+++ carma_message_utilities/carma_message_utilities/__init__.py
@@ -12,4 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .core import msg_from_yaml, msg_from_yaml_file  # noqa: F401 (import-unused error)
+from .file_io import (
+    msg_from_yaml,  # noqa: F401 (module imported but unused)
+    msg_from_yaml_file,  # noqa: F401 (module imported but unused)
+)

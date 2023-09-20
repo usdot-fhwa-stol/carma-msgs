196af1ab84aa00e0096eec32bee980f4be91e8f4
Adam Morrissett <adam.l.morrissett@leidos.com>
2023-09-20 13:40:03 -0400

    #220 Fix bug in msg_from_yaml_file
    The function did not have any checks when loading the data, so malformed
    files would cause the function to throw unhelpful errors. The function
    has been improved to throw meaning errors on malformed data or use
    default message values in some cases.

--- carma_message_utilities/carma_message_utilities/file_io.py
+++ carma_message_utilities/carma_message_utilities/file_io.py
@@ -12,22 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from pathlib import Path
+
 from rosidl_runtime_py import set_message_fields
 from rosidl_runtime_py.utilities import get_message
 
 import yaml
 
 
-def msg_from_yaml_file(filename: str):
-    with open(filename, "r") as file:
+def msg_from_yaml_file(path: Path):
+    with path.open() as file:
         return msg_from_yaml(file)
 
 
 def msg_from_yaml(stream):
     yaml_data = yaml.safe_load(stream)
 
+    if yaml_data is None:
+        raise TypeError("Could not load YAML data")
+
+    if "message_type" not in yaml_data:
+        raise KeyError(f"Message YAML file does not include `message_type`: {yaml_data}")
+
     msg_type = get_message(yaml_data["message_type"])
     msg = msg_type()
-    set_message_fields(msg, yaml_data["message_fields"])
+
+    if "message_fields" in yaml_data and yaml_data["message_fields"] is not None:
+        set_message_fields(msg, yaml_data["message_fields"])
 
     return msg

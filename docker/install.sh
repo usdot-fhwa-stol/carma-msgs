b30c698959a3bc1d3ac482e7694a220abfa5fc11
Misheel Bayartsengel <misheel.bayart@gmail.com>
2024-04-29 17:55:32 -0400

    revert rm

--- docker/install.sh
+++ docker/install.sh
@@ -20,11 +20,10 @@ rm -rf /home/carma/autoware.ai/
 
 # ROS1 message setup
 cd ~/.base-image/ros1_msgs_ws && source /opt/ros/noetic/setup.bash && colcon build --packages-up-to autoware_msgs cav_msgs cav_srvs j2735_msgs can_msgs carma_debug_msgs autoware_lanelet2_msgs carma_cooperative_perception_interfaces
-rm -rf build log logs src
+
 # ROS2 message setup
 cd ~/.base-image/ros2_msgs_ws && source /opt/ros/foxy/setup.bash && colcon build --packages-up-to autoware_msgs autoware_lanelet2_msgs can_msgs carma_debug_ros2_msgs carma_driver_msgs carma_localization_msgs carma_msgs carma_perception_msgs carma_planning_msgs carma_v2x_msgs j2735_v2x_msgs carma_cooperative_perception_interfaces
-rm -rf build log logs src
-df -h
+
 # Build the bridge
 source /opt/ros/noetic/setup.bash
 source /opt/ros/foxy/setup.bash

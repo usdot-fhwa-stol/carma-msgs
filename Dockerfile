0ba7a6e8386107559dde282b290ef9847fbcbbb5
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2024-09-11 11:07:37 -0400

    Merge release/omega (4.8.0) branch into master (#241)
    <!-- Thanks for the contribution, this is awesome. -->
    
    # PR Details
    This PR is to merge release/omega branch into master branch for C1t
    release with new version.
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related GitHub Issue
    
    <!--- This project only accepts pull requests related to open GitHub
    issues or Jira Keys -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please DO NOT name partially fixed issues, instead open an issue
    specific to this fix -->
    <!--- Please link to the issue here: -->
    
    ## Related Jira Key
    
    <!-- e.g. CAR-123 -->
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Defect fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that cause existing functionality
    to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have added any new packages to the sonar-scanner.properties file
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] I have read the
    [**CONTRIBUTING**](https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/Contributing.md)
    document.
    - [ ] I have added tests to cover my changes.
    - [ ] All new and existing tests passed.
    
    ---------
    
    Co-authored-by: Adam Morrissett <22103567+adamlm@users.noreply.github.com>
    Co-authored-by: paulbourelly999 <77466294+paulbourelly999@users.noreply.github.com>
    Co-authored-by: dev <paul.k.bourelly@leidos.com>
    Co-authored-by: Misheel Bayartsengel <misheel.bayart@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,30 @@
-FROM usdotfhwastol/carma-base:carma-system-4.5.0 as base_image
+ARG DOCKER_ORG=usdotfhwastoldev
+ARG DOCKER_TAG=develop
+FROM ${DOCKER_ORG}/carma-base:${DOCKER_TAG} as base_image
 SHELL ["/bin/bash", "-c"]
 
 ARG DEBIAN_FRONTEND="noninteractive"
+ARG VERSION="NULL"
+ARG VCS_REF="NULL"
+ARG BUILD_DATE="NULL"
+ARG GIT_BRANCH=develop 
 
-LABEL org.label-schema.schema-version="1.0"
-LABEL org.label-schema.name="carma-msgs"
-LABEL org.label-schema.description="carma msgs bridge for the CARMA Platform"
-LABEL org.label-schema.vendor="Leidos"
-LABEL org.label-schema.version=${VERSION}
-LABEL org.label-schema.url="https://highways.dot.gov/research/research-programs/CARMA"
-LABEL org.label-schema.vcs-url="https://github.com/usdot-fhwa-stol/carma-msgs"
-LABEL org.label-schema.vcs-ref=${VCS_REF}
-LABEL org.label-schema.build-date=${BUILD_DATE}
+LABEL org.label-schema.schema-version="1.0" \
+      org.label-schema.name="carma-msgs" \
+      org.label-schema.description="carma msgs bridge for the CARMA Platform" \
+      org.label-schema.vendor="Leidos" \
+      org.label-schema.version=${VERSION} \
+      org.label-schema.url="https://highways.dot.gov/research/research-programs/CARMA" \
+      org.label-schema.vcs-url="https://github.com/usdot-fhwa-stol/carma-msgs" \
+      org.label-schema.vcs-ref=${VCS_REF} \
+      org.label-schema.build-date=${BUILD_DATE}
 
 RUN  mkdir -p ~/.base-image/ros1_msgs_ws/src/carma_msgs
 RUN  mkdir -p /home/carma/src
 COPY . /home/carma/.base-image/ros1_msgs_ws/src/carma_msgs/
 RUN  mkdir -p ~/.base-image/ros2_msgs_ws/src/carma_msgs
 COPY . /home/carma/.base-image/ros2_msgs_ws/src/carma_msgs/
+
+RUN /home/carma/.base-image/ros1_msgs_ws/src/carma_msgs/docker/checkout.bash -b ${GIT_BRANCH} -r /home/carma/.base-image/workspace
+
 RUN /home/carma/.base-image/ros1_msgs_ws/src/carma_msgs/docker/install.sh
--- docker/checkout.bash
+++ docker/checkout.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#  Copyright (C) 2018-2021 LEIDOS.
-# 
+#  Copyright (C) 2018-2024 LEIDOS.
+#
 #  Licensed under the Apache License, Version 2.0 (the "License"); you may not
 #  use this file except in compliance with the License. You may obtain a copy of
 #  the License at
-# 
+#
 #  http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 #  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,11 +20,13 @@
 set -exo pipefail
 
 dir=~
+BRANCH=develop
 while [[ $# -gt 0 ]]; do
       arg="$1"
       case $arg in
-            -d|--develop)
-                  BRANCH=develop
+            -b|--branch)
+                  BRANCH=$2
+                  shift
                   shift
             ;;
             -r|--root)
@@ -32,6 +34,10 @@ while [[ $# -gt 0 ]]; do
                   shift
                   shift
             ;;
+            -d)  # This is used to ignore -d arg from build-image.sh to avoid conflict with -b arg used in dockerfile.
+                  shift
+            ;;
+            *)
       esac
 done
 
@@ -39,15 +45,22 @@ cd ${dir}/src
 
 
 # clone carma repos
-
-if [[ "$BRANCH" = "develop" ]]; then
+# Currently autoware.ai is being cloned into a hardcoded directory
+# that the install script is also hardcoded to look for. This should be uncoupled:
+# https://github.com/usdot-fhwa-stol/carma-platform/issues/2374
+if [[ "$BRANCH" == "develop" ]]; then
       cd /home/carma/
-      git clone https://github.com/usdot-fhwa-stol/autoware.ai.git --depth 1 --branch carma-system-4.5.0
-      cd ${dir}/src 
-      git clone --depth 1 --branch carma-system-4.5.0 https://github.com/usdot-fhwa-stol/ros1_bridge.git
+      git clone https://github.com/usdot-fhwa-stol/autoware.ai.git --depth 1 --branch carma-"$BRANCH"
+      cd ${dir}/src
+      git clone  https://github.com/usdot-fhwa-stol/ros1_bridge.git --depth 1 --branch "$BRANCH"
+elif [[ "$BRANCH" == "master" ]]; then
+      cd /home/carma/
+      git clone https://github.com/usdot-fhwa-stol/autoware.ai.git --depth 1 --branch carma-"$BRANCH"
+      cd ${dir}/src
+      git clone https://github.com/usdot-fhwa-stol/ros1_bridge.git  --depth 1 --branch carma-"$BRANCH"
 else
       cd /home/carma/
-      git clone https://github.com/usdot-fhwa-stol/autoware.ai.git --depth 1 --branch carma-system-4.5.0
-      cd /home/carma/.base-image/workspace/src
-      git clone --depth 1 --branch carma-system-4.5.0 https://github.com/usdot-fhwa-stol/ros1_bridge.git
+      git clone https://github.com/usdot-fhwa-stol/autoware.ai.git --depth 1 --branch "$BRANCH"
+      cd ${dir}/src
+      git clone https://github.com/usdot-fhwa-stol/ros1_bridge.git  --depth 1 --branch "$BRANCH"
 fi

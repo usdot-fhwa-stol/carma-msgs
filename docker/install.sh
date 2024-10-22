04ea195a174c4c7475dbd1340c0e79b53014ef24
John Chrosniak <chrosniakj@gmail.com>
2024-10-22 20:27:09 +0400

    Arc 159 Remove ROS2 Foxy Specific Code (#247)

--- docker/install.sh
+++ docker/install.sh
@@ -19,11 +19,11 @@ rm -rf /home/carma/autoware.ai/
 cd ~/.base-image/ros1_msgs_ws && source /opt/ros/noetic/setup.bash && colcon build --packages-up-to autoware_msgs cav_msgs cav_srvs j2735_v2x_msgs can_msgs carma_debug_msgs autoware_lanelet2_msgs carma_cooperative_perception_interfaces
 
 # ROS2 message setup
-cd ~/.base-image/ros2_msgs_ws && source /opt/ros/foxy/setup.bash && colcon build --packages-up-to autoware_msgs autoware_lanelet2_msgs can_msgs carma_debug_ros2_msgs carma_driver_msgs carma_localization_msgs carma_msgs carma_perception_msgs carma_planning_msgs carma_v2x_msgs j2735_v2x_msgs carma_cooperative_perception_interfaces
+cd ~/.base-image/ros2_msgs_ws && source /opt/ros/humble/setup.bash && colcon build --packages-up-to autoware_msgs autoware_lanelet2_msgs can_msgs carma_debug_ros2_msgs carma_driver_msgs carma_localization_msgs carma_msgs carma_perception_msgs carma_planning_msgs carma_v2x_msgs j2735_v2x_msgs carma_cooperative_perception_interfaces
 
 # Build the bridge
 source /opt/ros/noetic/setup.bash
-source /opt/ros/foxy/setup.bash
+source /opt/ros/humble/setup.bash
 source ~/.base-image/ros1_msgs_ws/install/local_setup.bash
 source ~/.base-image/ros2_msgs_ws/install/local_setup.bash
 cd ~/.base-image/workspace/src

6dfaf0f4915768d9ecd8b6a39d75279d4a91385e
Misheel Bayartsengel <misheel.bayart@gmail.com>
2025-02-10 17:55:31 -0500

    fix to add more msgs

--- docker/install.sh
+++ docker/install.sh
@@ -16,10 +16,10 @@ cp -R /home/carma/autoware.ai/jsk_recognition /home/carma/.base-image/ros2_msgs_
 rm -rf /home/carma/autoware.ai/
 
 # ROS1 message setup
-cd ~/.base-image/ros1_msgs_ws && source /opt/ros/noetic/setup.bash && colcon build --packages-up-to autoware_msgs cav_msgs cav_srvs j2735_v2x_msgs can_msgs carma_debug_msgs autoware_lanelet2_msgs carma_cooperative_perception_interfaces
+cd ~/.base-image/ros1_msgs_ws && source /opt/ros/noetic/setup.bash && colcon build --parallel-workers $(nproc) --packages-up-to autoware_msgs cav_msgs cav_srvs j2735_v2x_msgs can_msgs carma_debug_msgs autoware_lanelet2_msgs carma_cooperative_perception_interfaces
 
 # ROS2 message setup
-cd ~/.base-image/ros2_msgs_ws && source /opt/ros/foxy/setup.bash && colcon build --packages-up-to autoware_msgs autoware_lanelet2_msgs can_msgs carma_debug_ros2_msgs carma_driver_msgs carma_localization_msgs carma_msgs carma_perception_msgs carma_planning_msgs carma_v2x_msgs j2735_v2x_msgs carma_cooperative_perception_interfaces
+cd ~/.base-image/ros2_msgs_ws && source /opt/ros/foxy/setup.bash && colcon build --parallel-workers $(nproc) --packages-up-to raptor_dbw_msgs dbw_mkz_msgs autoware_msgs autoware_lanelet2_msgs can_msgs carma_debug_ros2_msgs carma_driver_msgs carma_localization_msgs carma_msgs carma_perception_msgs carma_planning_msgs carma_v2x_msgs j2735_v2x_msgs carma_cooperative_perception_interfaces
 
 # Build the bridge
 source /opt/ros/noetic/setup.bash
@@ -29,5 +29,5 @@ source ~/.base-image/ros2_msgs_ws/install/local_setup.bash
 cd ~/.base-image/workspace/src
 cd ../
 sudo apt-get update
-colcon build --event-handlers console_direct+ --packages-select ros1_bridge --cmake-force-configure
+colcon build --parallel-workers $(nproc) --event-handlers console_direct+ --packages-select ros1_bridge --cmake-force-configure
 sudo chmod -R ugo+x ~/.base-image/workspace/install
